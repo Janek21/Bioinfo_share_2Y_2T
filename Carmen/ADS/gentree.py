2fe900b407d7e4e9d0f051431a2468f796972b36
carmensat <144720995+carmensat@users.noreply.github.com>
2024-02-09 17:46:48 +0100

    Add files via upload
    Recursive traversal of a general tree

---
+++ Carmen/ADS/gentree.py
@@ -0,0 +1,36 @@
+from pytokr import pytokr
+
+s= pytokr()
+children= int(s())
+
+'''
+THIS IS NOT A BINARY TREE!!!!!
+FIRST NB IS THE NB OF NODES TOTAL. THEN SECOND NODE IS OUR ROOT, RIGHT AFTER IS THE NB OR CHILDREN. AND THEN WE START FROM THE LEFT THE CHILDREN AND KEEP MOVING ACROSS THE TREE 
+'''
+
+def reading_input():        #define the tree according to the size we get
+    l=int(s())  #take from the input the node we are on as integer
+    child=int(s())   #take its size as int 
+    children=[]
+    for x in range(child):
+        children.append(reading_input())
+    return [l, children]
+
+
+def post_order(inp):    
+    if not inp:     
+        return []  
+    res=[]
+    for c in inp[1]:
+        res.extend(post_order(c))
+    res.append(inp[0])
+    return (res)
+    
+    
+inp=reading_input()
+#print(inp)
+print('', *post_order(inp))
+
+#12
+#7 3 8 0 4 2 3 1 0 1 6 0 
+#5 0 2 4 9 0 1 0 8 0 5 0
