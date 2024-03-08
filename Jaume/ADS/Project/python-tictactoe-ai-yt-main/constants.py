60bcb280d37888d5b849d25dc677e21ce347e0b4
eloivilellaescolano <127297849+eloivilellaescolano@users.noreply.github.com>
2024-03-08 17:48:02 +0100

    jom 2.0

---
+++ Jaume/ADS/Project/python-tictactoe-ai-yt-main/constants.py
@@ -0,0 +1,32 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+LINE_COLOR = (23, 145, 135)
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
+
+w = [(CIRC_COLOR[pos] + CROSS_COLOR[pos])/2 for pos in range(3)]
+
+BG_COLOR = (w[0], w[1], w[2])
+LINE_COLOR = (w[0]-20, w[1]-20, w[2]-20)
+print(BG_COLOR)
